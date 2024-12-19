66d92eca69deb59ff52564dc162fcbfe618e7488
GiorgiGochitidze <113135317+GiorgiGochitidze@users.noreply.github.com>
2024-12-19 23:31:32 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿string fizzBuzzVal = "";
+
+for (var i = 1; i < 101; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+        fizzBuzzVal = "- FizzBuzz";
+    }
+    else if (i % 3 == 0)
+    {
+        fizzBuzzVal = "- Fizz";
+    }
+    else if (i % 5 == 0)
+    {
+        fizzBuzzVal = "- Buzz";
+    }
+    else
+    {
+        fizzBuzzVal = "";
+    }
+
+    Console.WriteLine($"{i} {fizzBuzzVal}");
+}
